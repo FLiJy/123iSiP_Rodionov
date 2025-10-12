5e1809a7a06011194664ce85d54c9dc4020dd639
FLiJ <FLiJx@FLIJX>
2025-10-12 19:21:19 +0300

    добавление класса person для абстракции общих хар-ик

--- 123iSiP_Rodionov/Program.cs
+++ 123iSiP_Rodionov/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityManagementSystem
+{
+    // Абстрактный класс Person для абстракции общих характеристик
+    public abstract class Person
+    {
+        private static int nextId = 1;
+
+        // Приватные поля для инкапсуляции
+        private int id;
+        private string name;
+        private int age;
+        private string contact;
