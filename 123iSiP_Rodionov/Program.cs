c2be3aa2e943967c9d704200339173f7011ede42
FLiJ <FLiJx@FLIJX>
2025-10-12 19:29:28 +0300

    метод для поиска по идентификатору

--- 123iSiP_Rodionov/Program.cs
+++ 123iSiP_Rodionov/Program.cs
@@ -146,7 +146,11 @@ namespace UniversityManagementSystem
         private List<Student> students = new List<Student>();
         private List<Teacher> teachers = new List<Teacher>();
         private List<Course> courses = new List<Course>();
-        // Методы для добавления
+      
         public void AddStudent(Student student) => students.Add(student);
         public void AddTeacher(Teacher teacher) => teachers.Add(teacher);
         public void AddCourse(Course course) => courses.Add(course);
+       
+        public Student FindStudentById(int id) => students.Find(s => s.Id == id);
+        public Teacher FindTeacherById(int id) => teachers.Find(t => t.Id == id);
+        public Course FindCourseById(int id) => courses.Find(c => c.Id == id);
